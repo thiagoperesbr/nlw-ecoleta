9f029135de18a1f6161f1202745413365f6bc382
Thiago Peres <thiago.mourao.peres@gmail.com>
2020-06-05 03:46:42 -0300

    Adicionando Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import path from 'path';
-// import cors from 'cors';
+import cors from 'cors';
 
 import routes from './routes';
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
